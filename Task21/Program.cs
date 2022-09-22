4f6f4e4b00d6d3480b3720d5601a4120c3c6521b
evgeny moskovskikh <mosevgwork@yandex.ru>
2022-09-23 01:19:42 +0300

    Да будет так

---
+++ Task21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты точки x1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки x2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки y2: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки z1: ");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+double s = Math.Sqrt(Math.Pow((x1-x2),2)+Math.Pow((y1-y2),2)+Math.Pow((z1-z2),2));
+Console.WriteLine($"Расстояние между двумя точками равно: {s:f2} ");
---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.Write(i * i * i + " ");
+}
